2a3e33c594ada2015edb047540c501fb04ec77be
Olivier Scholder <o.scholder@gmail.com>
2016-02-26 16:03:45 +0100

    Fix library error

--- STSviewer.py
+++ STSviewer.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from PyQt4.QtGui import QMainWindow, QApplication, QFileDialog
 from PyQt4 import QtGui, QtCore
 from GUI_STSviewer import Ui_MainWindow
@@ -6,7 +8,7 @@ import pyOmicron as pyO
 import re
 import matplotlib as mpl
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
+from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 import matplotlib.mlab as mlab
 

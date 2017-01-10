fd215a088159da01611c78308e5c45aee514bab9
Scholder <olivier.scholder@empa.ch>
2017-01-10 11:25:25 +0100

    Fix import for python2/3 compatibility

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
-import pyOmicron
-import STS
+try:
+	from pyOmicron import pyOmicron
+	from pyOmicron import STS
+except:
+	import pyOmicron
+	import STS
 
 __all__=["pyOmicron","STS"]
 __version__ = 0.1
